c43a28baf5c8025046aa9b69dc96f1c90a1ec0c1
wesly2000 <ylx.seu@foxmail.com>
2026-04-24 10:49:17 +0800

    feat: sys_trace

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -71,6 +71,9 @@ pub struct TaskControlBlockInner {
 
     /// Program break
     pub program_brk: usize,
+
+    /// Syscall count table
+    pub task_syscall_counts: [u16; 512],
 }
 
 impl TaskControlBlockInner {
@@ -137,6 +140,7 @@ impl TaskControlBlock {
                     program_brk: user_sp,
                 })
             },
+            task_syscall_counts: [0; 512]
         };
         // prepare TrapContext in user space
         let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
