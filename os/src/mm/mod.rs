e35ed12546d205660f7e20f76ff0453d060435fc
wesly2000 <ylx.seu@foxmail.com>
2026-04-25 12:47:22 +0800

    feat: address validation
    The Read/Write operation requires further check on the permission
    of its target address, which includes:
    
    1. Operation consistency: for example, a write op is allowed only on
    writable page;
    2. Valid range: in SV39 address system, the user is allowed only to
    operate when 63..38 bits are set to 0;
    3. User accessibility: the target page must have V (valid) and U (user)
    flags set.

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -16,6 +16,7 @@ use address::{StepByOne, VPNRange};
 pub use frame_allocator::{frame_alloc, FrameTracker};
 pub use memory_set::remap_test;
 pub use memory_set::{MapPermission, MemorySet, KERNEL_SPACE};
+pub use memory_set::{PROT_R, PROT_W, PROT_X};
 pub use page_table::{translated_byte_buffer, translated_refmut, translated_str, PageTableEntry};
 use page_table::{PTEFlags, PageTable};
 /// initiate heap allocator, frame allocator and kernel space
