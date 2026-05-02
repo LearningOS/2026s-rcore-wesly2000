66ed859e81395f3f3857d11ffc738e10e9127e6d
wesly2000 <ylx.seu@foxmail.com>
2026-05-02 21:35:44 +0800

    feat: stride-based scheduling

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -202,10 +202,11 @@ pub fn sys_spawn(path: *const u8) -> isize {
 }
 
 // YOUR JOB: Set task priority.
-pub fn sys_set_priority(_prio: isize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_set_priority(prio: isize) -> isize {
+    if prio <= 1 {
+        error!("Priority must be >= 2");
+        return -1;
+    }
+    current_task().unwrap().inner_exclusive_access().set_priority(prio);
+    prio
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -38,6 +38,13 @@ impl TaskControlBlock {
     }
 }
 
+const BIG_STRIDE: usize = 1 << 16;
+/// Initial stride
+const INIT_STRIDE: usize = 16;
+/// Initial priority
+const INIT_PRIO: usize = 2;
+
+
 pub struct TaskControlBlockInner {
     /// The physical page number of the frame where the trap context is placed
     pub trap_cx_ppn: PhysPageNum,
@@ -72,8 +79,11 @@ pub struct TaskControlBlockInner {
     /// Program break
     pub program_brk: usize,
 
-    /// Syscall count table
-    pub task_syscall_counts: [u16; 512],
+    /// Stride value for scheduling, 0 (default) for the new one
+    stride: usize,
+
+    /// Priority for scheduling
+    priority: usize,
 }
 
 impl TaskControlBlockInner {
@@ -96,6 +106,21 @@ impl TaskControlBlockInner {
             self.fd_table.push(None);
             self.fd_table.len() - 1
         }
+    }
+    pub fn increase_stride(&mut self) {
+        self.stride += BIG_STRIDE / self.priority;
+    }
+    /// get stride
+    pub fn stride(&self) -> usize {
+        self.stride
+    }   
+    pub fn set_priority(&mut self, prio: isize) -> isize{    
+        if prio <= 1 {
+            return -1;
+        }    
+
+        self.priority = prio as usize;
+        prio
     }
 }
 
@@ -138,9 +163,10 @@ impl TaskControlBlock {
                     ],
                     heap_bottom: user_sp,
                     program_brk: user_sp,
+                    stride: INIT_STRIDE,
+                    priority: INIT_PRIO,
                 })
             },
-            task_syscall_counts: [0; 512]
         };
         // prepare TrapContext in user space
         let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
@@ -220,6 +246,8 @@ impl TaskControlBlock {
                     fd_table: new_fd_table,
                     heap_bottom: parent_inner.heap_bottom,
                     program_brk: parent_inner.program_brk,
+                    stride: INIT_STRIDE,
+                    priority: INIT_PRIO,
                 })
             },
         });
