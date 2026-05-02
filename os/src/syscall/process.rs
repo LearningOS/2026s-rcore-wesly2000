f22e0e1f7b214530857cdeb56394d970c8c9b5b9
wesly2000 <ylx.seu@foxmail.com>
2026-05-02 21:31:14 +0800

    feat: sys_spawn
    The difference between spawn and fork+exec is that we don't to the
    Copy-and-Write operation to the space allocated to the child process,
    which only includes:
    
    1. Create empty PCB;
    2. Load ELF to the PCB and build map;
    3. Set the sp and other context;
    4. Add the child to parents trace vector, and add it to the scheduler.
    
    spawn is much preferred on platform lack of resource.

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -50,7 +50,7 @@ fn write_mem(ptr: *const u8, src: &[u8]) {
 
 /// Read from address with len bytes to `dst`. It handles user page table fetching,
 /// va to pa translation where segmentation might exist.
-fn read_mem(ptr: *const u8, dst: &mut [u8]) {
+fn _read_mem(ptr: *const u8, dst: &mut [u8]) {
     let token = current_user_token();
     let buffers = translated_byte_buffer(token, ptr, dst.len());
     let mut offset = 0;
@@ -176,12 +176,29 @@ pub fn sys_sbrk(size: i32) -> isize {
 
 /// YOUR JOB: Implement spawn.
 /// HINT: fork + exec =/= spawn
-pub fn sys_spawn(_path: *const u8) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_spawn NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_spawn(path: *const u8) -> isize {
+    // Try to load ELF data
+    trace!("kernel:pid[{}] sys_spawn", current_task().unwrap().pid.0);
+    let token = current_user_token();
+    let path = translated_str(token, path);
+
+    if let Some(data) = get_app_data_by_name(path.as_str()) {
+        // Create new child task
+        let current_task = current_task().unwrap();
+        let new_task = current_task.spawn(data);
+        let new_pid = new_task.pid.0;
+        // modify trap context of new_task, because it returns immediately after switching
+        let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
+        // we do not have to move to next instruction since we have done it before
+        // for child process, spawn returns 0
+        trap_cx.x[10] = 0;
+        // add new task to scheduler
+        add_task(new_task);
+        new_pid as isize
+    } else {
+        // If anything goes wrong, return -1 to indicate failure
+        -1
+    }
 }
 
 // YOUR JOB: Set task priority.
